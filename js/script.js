614bd8104af8156c64605b1eb67900ad8dd208df
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-02-20 10:26:59 +0100

    Update script.js
    updating the list inside the array.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,17 @@
-// Create a new variable called pokemonList and assign a blank array to it
-let pokemonList = [];
-
-// Add Pokémon objects (at list 3) to the array
+let pokemonList = [
 {
   name: "Venusaur",
   height: 7,
   types: ['grass', 'poison']
-};
-
+},
 {
   name: "Charmander",
   height: 6,
   types: ['fire']
-};
-
+},
 {
   name: "Squirtle",
   height: 5,
   types: ['water']
-};
-
-// Display the pokemonList array
-console.log(pokemonList);
+}
+];
