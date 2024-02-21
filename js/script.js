9df368176cf120fa685e1d188c5c3d10ac8e6e10
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-02-21 10:27:16 +0100

    Update script.js
    Changed the value of pokemonList[i].height

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ let pokemonList = [
   ];
   for (let i = 0; i < pokemonList.length; i++) {
     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
-    if (pokemonList[i].height <= 1.0) {
+    if (pokemonList[i].height < 5) {
       document.write('<br>');
     }
     else {
