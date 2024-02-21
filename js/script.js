c5905561544cd60afdae643ed830e21624aa9897
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-02-21 10:28:47 +0100

    Update script.js
    minor changes to list description and names of pokemons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// describtions of pokemon lists
 let pokemonList = [
   {
     name: "Venusaur",
@@ -15,13 +16,16 @@ let pokemonList = [
     types: ['water']
   }
   ];
-  for (let i = 0; i < pokemonList.length; i++) {
+  // print the list of names from pokemonList and coresponding height of that name 
+  for (let i = 0; i < pokemonList.length; i++)
+  // print a message if the height is bigger than 5 
+ {
     document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
     if (pokemonList[i].height < 5) {
       document.write('<br>');
     }
     else {
-      document.write('That\'s a big pokemon!');
+      document.write('Wow, that\'s a big pokemon!');
       document.write('<br>');
     }
   }
