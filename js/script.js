59d505194324f209457dd53af694ffdbdd94aa7d
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-02-21 10:08:21 +0100

    Update script.js
    Nested a for loop that iterates over each item in pokemonList:
    
    Used document.write() inside the loop’s code to write the Pokémon name on my website’s DOM and wrote the Pokémon’s height next to its name, for example, “Vanusaur (height: 7)”.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 let pokemonList = [
-{
-  name: "Venusaur",
-  height: 7,
-  types: ['grass', 'poison']
-},
-{
-  name: "Charmander",
-  height: 6,
-  types: ['fire']
-},
-{
-  name: "Squirtle",
-  height: 5,
-  types: ['water']
-}
-];
+  {
+    name: "Venusaur",
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: "Charmander",
+    height: 6,
+    types: ['fire']
+  },
+  {
+    name: "Squirtle",
+    height: 5,
+    types: ['water']
+  }
+  ];
+  for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+    if (pokemonList[i].height <= 1.0) {
+      document.write('<br>');
+    }
+    else {
+      document.write('That\'s a big pokemon!');
+      document.write('<br>');
+    }
+  }
