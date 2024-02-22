ae507ed8c6b929399bf8c882dfde615e03df8fa6
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-02-22 20:50:06 +0100

    Update script.js
    printimg the list of names from pokemonList and corresponding height of that names using forEach() function instead of the for()

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // describtions of pokemon lists
+
 let pokemonList = [
   {
     name: "Venusaur",
@@ -15,17 +16,15 @@ let pokemonList = [
     height: 5,
     types: ['water']
   }
-  ];
-  // print the list of names from pokemonList and coresponding height of that name 
-  for (let i = 0; i < pokemonList.length; i++)
-  // print a message if the height is bigger than 5 
- {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
-    if (pokemonList[i].height < 5) {
-      document.write('<br>');
-    }
-    else {
-      document.write('Wow, that\'s a big pokemon!');
-      document.write('<br>');
-    }
+];
+
+// print the list of names from pokemonList and corresponding height of that name using forEach()
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + ' (height: ' + pokemon.height + ') ');
+
+  if (pokemon.height > 5) {
+    document.write('Wow, that\'s a big pokemon!');
   }
+
+  document.write('<br>');
+});
